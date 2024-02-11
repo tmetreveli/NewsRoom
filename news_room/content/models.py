5c95404c659dbeafc9439b172418e2b073f6ef32
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-02-11 14:06:32 +0400

    created content app and configured category, tag and article models

---
+++ news_room/content/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    logo = models.ImageField(upload_to='category_logos/')
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=320)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    publication_datetime = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    main_image = models.ImageField(upload_to='article_images/')
+    publishing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
